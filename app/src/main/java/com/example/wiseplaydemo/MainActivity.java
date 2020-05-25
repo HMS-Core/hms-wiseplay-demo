87a5606b690f236f3ad7486326b4033fda27f101
leonard-hw <65204583+leonard-hw@users.noreply.github.com>
2020-05-25 12:12:50 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/wiseplaydemo/MainActivity.java
+++ app/src/main/java/com/example/wiseplaydemo/MainActivity.java
@@ -16,22 +16,13 @@
 
 package com.example.wiseplaydemo;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.UUID;
-
-import android.icu.text.RelativeDateTimeFormatter;
 import android.media.DeniedByServerException;
 import android.media.MediaDrm;
 import android.media.NotProvisionedException;
 import android.media.ResourceBusyException;
 import android.media.UnsupportedSchemeException;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -39,6 +30,24 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.UUID;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -52,7 +61,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * Wiseplay PSSH box
      */
-    private static final String PSSH_BOX = "AAAAo3Bzc2gAAAAAPV5tNZuaQei4Qz88bnI/LAAAAIN7InZlcnNpb24iOiJWMS4wIiwiY29udGVudElEIjoiTVRBd01EQXhNREV5TXpRMU5qYzRPUT09Iiwia2lkcyI6WyJOelpsTnpSaU56YzBaREF4TkRSaU1XSXhPRE5tTlRnME1ERTRabVEzTVRrPSJdLCJlbnNjaGVtYSI6ImNlbmMifQ==";
+    private static final String PSSH_BOX = "AAAAY3Bzc2gBAAAAPV5tNZuaQei4Q908bnLELAAAAAO7vqaHr+24kInNiIm9y7x3iQi++lh914kkW+r9JHZ1u3iSe67fx4l6kSNlOHvv2soAAAAPc2Zhc1JBZXV5SEpGU0pB";
+
+    /**
+     * url of get license
+     */
+    private String licenseUrl = "https://wiseplay.hwcloudtest.cn:9091/playdemo/chinaDrm/getLicense?contentId=sfasRAeuyHJFSJA&useDrmProxy=1";
 
     /**
      * button of get license
@@ -75,11 +89,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button deleteLicense;
 
     /**
-     * url of get license
-     */
-    private String licenseUrl = "http://117.78.34.30:8000/wisePlayDrm/getLicense";
-
-    /**
      * keySetId from last offline license fetching
      */
     private byte[] offlineKeySetId = null;
@@ -89,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        handleSSLHandshake();
     }
 
     private void initView() {
@@ -107,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.down_online_license:
-                new Thread(new Runnable(){
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
                         getOnlineLicense();
@@ -117,7 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.down_offline_license:
-                new Thread(new Runnable(){
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
                         getOfflineLicense();
@@ -126,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.use_offline_license:
-                new Thread(new Runnable(){
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
                         useOfflineLicense();
@@ -136,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.delete_license:
-                new Thread(new Runnable(){
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
                         deleteLicense();
@@ -150,15 +160,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void deleteLicense() {
         if (!MediaDrm.isCryptoSchemeSupported(WISEPLAY_DRM_UUID)) {
-            Toast.makeText(MainActivity.this, "The device does not support wiseplay drm.", Toast.LENGTH_SHORT).show();
+            showToast("The device does not support wiseplay drm.");
             return;
         }
         HttpURLConnection connection = null;
         DataOutputStream outputStream = null;
         InputStream inputStream = null;
+        MediaDrm mediaDrm = null;
+        byte[] sessionID = null;
+
         try {
-            MediaDrm mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
-            byte[] sessionID = mediaDrm.openSession();
+            mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
+            sessionID = mediaDrm.openSession();
 
             // Obtain the keySetId of the license from the store.
             byte[] keySetIdToDelete = offlineKeySetId;
@@ -166,9 +179,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mediaDrm.getKeyRequest(keySetIdToDelete, null, null, MediaDrm.KEY_TYPE_RELEASE, null);
 
             byte[] requestData = keyRequest.getData();
-            String licenseServerUrl = keyRequest.getDefaultUrl();
-
-            URL url = new URL(licenseServerUrl);
+            URL url = new URL(licenseUrl);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setDoOutput(true);
@@ -184,17 +195,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 inputStream = connection.getInputStream();
                 byte[] response = toByteArray(inputStream);
 
-                mediaDrm.provideKeyResponse(sessionID, response);
+                mediaDrm.provideKeyResponse(keySetIdToDelete, response);
 
                 // Delete the cached license relationship from the store.
                 offlineKeySetId = null;
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "delete offline license success.", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                Log.i(TAG, "delete response : " + new String(response, StandardCharsets.UTF_8));
+                showToast("delete offline license success.");
             }
 
         } catch (NotProvisionedException | UnsupportedSchemeException | ResourceBusyException | IOException | DeniedByServerException e) {
@@ -211,6 +217,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (connection != null) {
                     connection.disconnect();
                 }
+                if (mediaDrm != null) {
+                    if (sessionID != null) {
+                        mediaDrm.closeSession(sessionID);
+                    }
+                    mediaDrm.close();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -219,25 +231,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void useOfflineLicense() {
         if (!MediaDrm.isCryptoSchemeSupported(WISEPLAY_DRM_UUID)) {
-            Toast.makeText(MainActivity.this, "The device does not support wiseplay drm.", Toast.LENGTH_SHORT).show();
+            showToast("The device does not support wiseplay drm.");
             return;
         }
         // Obtain the keySetId of the license from the store.
         byte[] keySetIdToRestore = offlineKeySetId;
+        MediaDrm mediaDrm;
+        byte[] sessionID;
         try {
-            MediaDrm mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
-            byte[] sessionID = mediaDrm.openSession();
+            mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
+            sessionID = mediaDrm.openSession();
             mediaDrm.restoreKeys(sessionID, keySetIdToRestore);
 
             // If resotreKeys succeed, you can play it with this license.
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(MainActivity.this, "use offline license success.", Toast.LENGTH_SHORT).show();
-                }
-            });
-
+            showToast("use offline license success.");
+            mediaDrm.closeSession(sessionID);
+            mediaDrm.close();
         } catch (NotProvisionedException | UnsupportedSchemeException | ResourceBusyException e) {
             e.printStackTrace();
             Log.i(TAG, "use offline license failed: " + e.getMessage());
@@ -246,18 +256,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void getOfflineLicense() {
         if (!MediaDrm.isCryptoSchemeSupported(WISEPLAY_DRM_UUID)) {
-            Toast.makeText(MainActivity.this, "The device does not support wiseplay drm.", Toast.LENGTH_SHORT).show();
+            showToast("The device does not support wiseplay drm.");
             return;
         }
         HttpURLConnection connection = null;
         DataOutputStream outputStream = null;
         InputStream inputStream = null;
+        MediaDrm mediaDrm = null;
+        byte[] sessionID = null;
+
         try {
             // Obtain the value of pssh box of the movie whose UUID is wiseplay drm from the movie.
             byte[] initData = Base64.decode(PSSH_BOX, Base64.DEFAULT);
             String mimeType = "video/mp4";
-            MediaDrm mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
-            byte[] sessionID = mediaDrm.openSession();
+            mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
+            sessionID = mediaDrm.openSession();
             MediaDrm.KeyRequest keyRequest =
                     mediaDrm.getKeyRequest(sessionID, initData, mimeType, MediaDrm.KEY_TYPE_OFFLINE, null);
 
@@ -283,13 +296,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 // Save the keySetId of the offline license to the store.
                 offlineKeySetId = keySetId;
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "get offline license success.", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                Log.i(TAG, "getOfflineLicense response : " + new String(response, StandardCharsets.UTF_8));
+                showToast("get offline license success.");
             }
         } catch (NotProvisionedException | UnsupportedSchemeException | ResourceBusyException | IOException | DeniedByServerException e) {
             e.printStackTrace();
@@ -305,6 +313,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (connection != null) {
                     connection.disconnect();
                 }
+                if (mediaDrm != null) {
+                    if (sessionID != null) {
+                        mediaDrm.closeSession(sessionID);
+                    }
+                    mediaDrm.close();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -313,20 +327,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void getOnlineLicense() {
         if (!MediaDrm.isCryptoSchemeSupported(WISEPLAY_DRM_UUID)) {
-            Toast.makeText(MainActivity.this, "The device does not support wiseplay drm.", Toast.LENGTH_SHORT).show();
+            showToast("The device does not support wiseplay drm.");
             return;
         }
         HttpURLConnection connection = null;
         DataOutputStream outputStream = null;
         InputStream inputStream = null;
+        MediaDrm mediaDrm = null;
+        byte[] sessionID = null;
+
         try {
             // Value of pssh box obtained from the movie.
             byte[] initData = Base64.decode(PSSH_BOX, Base64.DEFAULT);
 
             // Obtain the value of pssh box of the movie whose UUID is wiseplay drm from the movie.
             String mimeType = "video/mp4";
-            MediaDrm mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
-            byte[] sessionID = mediaDrm.openSession();
+            mediaDrm = new MediaDrm(WISEPLAY_DRM_UUID);
+            sessionID = mediaDrm.openSession();
             MediaDrm.KeyRequest keyRequest =
                     mediaDrm.getKeyRequest(sessionID, initData, mimeType, MediaDrm.KEY_TYPE_STREAMING, null);
 
@@ -347,15 +364,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 inputStream = connection.getInputStream();
                 byte[] response = toByteArray(inputStream);
-
+                Log.i(TAG, "getOnlineLicense response : " + new String(response, StandardCharsets.UTF_8));
                 mediaDrm.provideKeyResponse(sessionID, response);
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "get online license success.", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                showToast("get online license success.");
             }
         } catch (NotProvisionedException | IOException | DeniedByServerException | ResourceBusyException | UnsupportedSchemeException e) {
             e.printStackTrace();
@@ -371,6 +383,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (connection != null) {
                     connection.disconnect();
                 }
+                if (mediaDrm != null) {
+                    if (sessionID != null) {
+                        mediaDrm.closeSession(sessionID);
+                    }
+                    mediaDrm.close();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -385,5 +403,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             outputStream.write(buffer, 0, bytesRead);
         }
         return outputStream.toByteArray();
+    }
+
+    private void showToast(final String message) {
+        if (Looper.getMainLooper().getThread() == Thread.currentThread()) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        } else {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            // trustAllCerts信任所有的证书
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 }
